35c4d0790c26e6a0c5c8de6d00d87dec7ef059c9
Balazs Gollner <Balazs_Gollner@epam.com>
2016-03-02 21:37:45 +0100

    Tweaking the side menu

--- OpenHASWeb/routes/index.js
+++ OpenHASWeb/routes/index.js
@@ -5,10 +5,13 @@ var nodeManager = require('../dao/node_manager')
 
 /* GET home page. */
 router.get('/',auth.ensureAuthenticated ,function(req, res) {
+  res.redirect('/dashboard')
+})
+
+router.get('/dashboard', auth.ensureAuthenticated, function(req, res) {
   nodeManager.favouriteNodes(function(nodes){
     res.render('index',{nodes:nodes});
   })
-
-});
+})
 
 module.exports = router;
