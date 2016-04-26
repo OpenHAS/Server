d2bcb4d2b0626ab9bf211169044dee28c4cf779f
Balazs Gollner <Balazs_Gollner@epam.com>
2016-04-26 21:55:04 +0200

    Adding indexes to events

--- OpenHASWeb/dao/models/event.js
+++ OpenHASWeb/dao/models/event.js
@@ -7,7 +7,20 @@ var Event = new Schema({
   parameters : [String]
 })
 
+
+Event.index({source: 1})
+Event.index({timestamp: -1})
+
 var EventModel = mongoose.model('Event', Event);
+
+EventModel.on('index', function(error) {
+  var errorMessage = "No error"
+  if (error != undefined) {
+    errorMessage = error
+  }
+
+  console.log('Indexes have been ensured on Event. Error: '+errorMessage)
+});
 
 module.exports.Schema = Event
 module.exports.Model = EventModel
