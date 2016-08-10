8b0a73f15ee4b87e0c57f397d73e9fbd7f887107
Balazs Gollner <Balazs_Gollner@epam.com>
2016-08-10 22:44:24 +0200

    Adding HTTPS support

--- OpenHASWeb/config.js
+++ OpenHASWeb/config.js
@@ -9,4 +9,8 @@ config.mqtt.username = process.env.MQTT_USER
 config.mqtt.password = process.env.MQTT_PASSWORD
 config.mqtt.rootTopic = "/openhas"
 
+config.openssl = {}
+config.openssl.privatekey = 'sslcert/server.key'
+config.openssl.certificate = 'sslcert/server.cer'
+
 module.exports = config
